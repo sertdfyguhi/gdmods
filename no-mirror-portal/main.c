6d89f1db889641e0601cb06579db3e685d773d97
sertdfyguhi-git <gdkamtalan@gmail.com>
2021-11-04 07:57:17 +0800

    better no-mirror-portal

--- no-mirror-portal/main.c
+++ no-mirror-portal/main.c
@@ -6,14 +6,14 @@
 void inject() __attribute__((constructor));
 
 uintptr_t getBase() {
-  return _dyld_get_image_vmaddr_slide(0)+0x100000000;
+	return _dyld_get_image_vmaddr_slide(0)+0x100000000;
 }
 
 void inject() {
-  uintptr_t base = getBase();
-  char byte[1] = {0xC3};
+	uintptr_t base = getBase();
+	char byte[1] = {0xC3};
 
-  // make PlayLayer::toggleFlipped do nothing
-  mach_vm_protect(mach_task_self(), base+0x7bdc0, 1, FALSE, 7 | VM_PROT_COPY);
-  mach_vm_write(mach_task_self(), base+0x7bdc0, &byte, 1);
+	// make PlayLayer::toggleFlipped do nothing
+	mach_vm_protect(mach_task_self(), base+0x7bdc0, 1, FALSE, 7 | VM_PROT_COPY);
+	mach_vm_write(mach_task_self(), base+0x7bdc0, &byte, 1);
 }
